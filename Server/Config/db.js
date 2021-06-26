a5cd05e439def29425f6126b94c2b3a8e9a5b4af
System Administrator <root@Mohammeds-MacBook-Pro.local>
2021-06-25 23:48:03 -0400

    database update

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
-//exports.LocalURI = "mongodb://localhost/books229";
+exports.LocalURI = "mongodb://localhost/books229";
 exports.RemoteURI = "mongodb+srv://mohammed:GKUQNML3YE46ckbA@clustertest.42so1.mongodb.net/book_store?retryWrites=true&w=majority";
 exports.HostName = "LocalHost";
 exports.Secret = "someSecret";
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
+export const RemoteURI = "mongodb+srv://mohammed:GKUQNML3YE46ckbA@clustertest.42so1.mongodb.net/book_store?retryWrites=true&w=majority"
 export const HostName = "LocalHost";
 export const Secret = "someSecret";
